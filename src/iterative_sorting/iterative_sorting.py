d772096887242733109ca4c4c5bd4e6a1fb987e4
Michael Stevens <stevensmike36@yahoo.com>
2020-04-14 08:06:54 -0500

    selection, bubble sort added to iterative_sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,25 +4,53 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        # use first number in list and 
+        #loop over right side starting with the next index      
+        for next_smallest in range(cur_index+1, len(arr)):
+         # is next_smallest < than right side of arr--smallest_index
+            if arr[next_smallest] < arr[smallest_index]:
+        # make new index equal to 
+                smallest_index =  next_smallest           
+        # TO-DO: swap a,b = b,a
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+
+    return arr #make sure you are returning in the proper place
 
 
+# TO-DO:  implement the Bubble Sort function below O(n^2)
 
-        # TO-DO: swap
-
-
-
-
-    return arr
-
-
-# TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for pair in range(len(arr)): #should be using a while loop?--while True:
+    #loop through elements n-1    
+        for i in range(len(arr) - 1): 
+            pair = i
+    #compare elements if arr[i] is greater move to the right    
+            if arr[pair] > arr[pair + 1]:
+    #swap them a,b = b,a
+               arr[pair], arr[pair + 1] = arr[pair + 1], arr[pair]
+               
     return arr
 
+#INSERTION SORTING
+
+#def insertion_sort(arr):
+    # Divide your hand into sorted on the left and unsorted on the right
+    # Sorted is just the first element
+    # then go card by card and move them into place.
+    # Loop through all elements in unsorted...
+#    for i in range(1, len(arr)):
+#        temp = arr[i]
+#        j = i  # j is our sliding index
+        # Shift sorted to the right until correct position found
+#        while j > 0 and temp < arr[j - 1]:
+#            arr[j] = arr[j - 1]  # Slide over one element
+ #           j -= 1
+        # Insert at that position
+#        arr[j] = temp
+#    return arr
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
